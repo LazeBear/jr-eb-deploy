94cc5a7edac0b0db79a6eac7a65112365fee084b
Mason Xiong <mason.xiong@yahoo.com>
2019-06-07 11:53:22 +1000

    connect with db

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 exports.connectToDB = () => {
-  const { DB_HOST, DB_PORT, DB_DATABASE } = process.env;
+  const { DB_HOST, DB_PORT, DB_DATABASE, DB_PASSWORD, DB_USER } = process.env;
   // mongoose.set('debug', true);
-  const connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
-
+  let connectionString;
+  if (DB_PASSWORD && DB_USER) {
+    connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}?retryWrites=true`;
+  } else {
+    connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+  }
   console.log(`Connecting to ${connectionString}`);
   mongoose.set('useFindAndModify', false);
   return mongoose.connect(connectionString, {
